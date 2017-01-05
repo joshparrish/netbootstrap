a21c711efd402fee7ccbe3ca111748f2b40d34ec
joshparrish <jparrish@astate.edu>
2017-01-05 11:41:39 -0600

    Update ubuntu_bootstrap.sh

--- ubuntu_bootstrap.sh
+++ ubuntu_bootstrap.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 
 apt-get update -y
-apt-get upgrade -y
-wget http://apt.puppetlabs.com/puppetlabs-release-pc1-xenial.deb
-dpkg -i puppetlabs-release-pc1-xenial.deb
-apt-get update -y
-apt-get -y install puppet
-apt -y install mcollective-client
-
-puppet config set server cfaserv.astate.edu
-puppet config set environment production
-puppet agent --enable
-puppet agent -t --waitforcert 30 >> /var/log/puppetagent.log
-
-reboot
+mkdir -p /usr/local/docker/web
+wget -qO- https://get.docker.com/ | sh
 
 
